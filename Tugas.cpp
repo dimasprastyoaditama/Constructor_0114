fd95c930d39fcaf1b21fd30b3204e525a2095895
dimasprastyoaditama <prastyod647@gmail.com>
2025-05-13 18:20:26 +0700

    first.commit

---
+++ Tugas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi class BangunDatar
+class BangunDatar;
+
+class persegiPanjang{
+    public:
+        void inputData(BangunDatar &bd);
+        void outputData(BangunDatar &bd);
+};
+
+
+   
+
