fe6034d2e6d232c8057e9a9324ec1a3c36cca1b4
dimasprastyoaditama <prastyod647@gmail.com>
2025-05-20 19:05:34 +0700

    Tambahkan Program C++ untuk kelas barang dengan construktor dan fungsi tampilan

---
+++ file1.cpp
@@ -0,0 +1,35 @@
+//Buatlah program c++ dengan class Barang yang memiliki atribut: namaBarang,
+//kodeBarang
+//program memiliki construktor berparameter untuk mengisi
+//nilai namaBarang dan kodeBarang.
+//serta memiliki fungsi untuk menampilkan informasi barang.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class barang {
+    public:
+    string namaBarang;
+    int kodeBarang;
+    barang(string nama, int kode) {
+        namaBarang = nama;
+        kodeBarang = kode;
+    }
+    void barang:: cetak() {
+        cout << "Nama Barang: " << namaBarang << endl;
+        cout << "Kode Barang: " << kodeBarang << endl;
+    }
+    int main (){
+        barang brg1 ("buku", 20000); //nama barang dan kode barang
+        barang brg2 ("pulpen", 15000); //nama barang dan kode
+        barang brg3 ("penghapus", 5000); //nama barang dan kode barang
+
+        cout << "Barang 1: " ; brg1.cetak();
+        cout << "Barang 2: " ; brg2.cetak();
+        cout << "Barang 3: " ; brg3.cetak();
+
+        return 0;
+    }
+
+};
